6cb4ccd6c337a5f6fda39887370438ce7e15a3e0
Brayan Esteves <brayan.esteves93@gmail.com>
2023-08-11 09:49:56 -0400

    Challenge Intermedio - Draw Square [11.08.2023]

---
+++ src/exercices/mine/basic/draw_square.js
@@ -0,0 +1,31 @@
+/**
+ * ENGLISH:
+ */
+
+
+// ============================================== //
+
+/**
+ * SPANISH:
+ * Dibujar un cuadrado hueco con asteríscos.
+ * 
+ * Ejemplos:
+ * cuadrado(4);
+ * 
+ * Devuelve:
+ *    ****
+ *    *  *
+ *    *  *
+ *    ****
+ * 
+ * Cómo hacerlo:
+ * - Función para crear el lado de arriba y abajo del cuadrado.
+ * - Función que haga los lados y el hueco del cuadrado.
+ * - Bucle de '0' al tamaño del lado menos 2 para equilibrar.
+ * | con él lado de arriba y abajo.
+ * - Bucle para pintar los espacios, menos 2 para equilibrar
+ * | con el asterístico de izquierda y derecha.
+ * - Ir concatenando en una variable cada línea del cuadrado.
+ * - Mostrar el cuadrado en la función principal.
+ */
+
