d222879979037e313b517f59e1ef44916e144d95
Brayan Esteves <brayan.esteves93@gmail.com>
2023-11-22 21:35:05 -0400

    Un programador de Argetina (Task) - FizzBuzz [22/11/2023]

---
+++ src/exercices/un-programador-de-argentina/easy/FizzBuzz/index.js
@@ -0,0 +1,40 @@
+/**
+ * [ENGLISH]
+ * FizzBuzz
+ * Write a <b>loop</b> that prints the numbers from 1 to 17 to the console.
+ * It must meet the following conditions: if the number to be printed is divisible by 3,
+ * it must print the <i><b>string</b></i> <b>'Fizz'</b> to the console.
+ * Instead, if it is divisible by 5, it should show: <b>'Buzz'</b>.
+ * Finally, if it is divisible by both it should show: <b>'FizzBuzz'</b>.
+ * Example:
+ * 1
+ * 2
+ * Fizz
+ * 4
+ * Buzz
+ * Fizz
+ * ...
+ * 14
+ * FizzBuzz
+ * 16
+ * =========
+ * [SPANISH]
+ * FizzBuzz
+ * Escribí un <b>loop</b> que imprima en la consola los números del 1 al 17.
+ * Deberá cumplir las siguientes condiciones: si el número a imprimir es multiplo de 3,
+ * debe mostrar en la consola el <i><b>string</b></i> <b>'Fizz'</b>.
+ * En cambio, si es múltiplo de 5, debe mostrar: <b>'Buzz'</b>.
+ * Por último, si es múltiplo de ambos debe mostrar: <b>'FizzBuzz'</b>.
+ * Ejemplo:
+ * 1
+ * 2
+ * Fizz
+ * 4
+ * Buzz
+ * Fizz
+ * ...
+ * 14
+ * FizzBuzz
+ * 16
+ */
+
