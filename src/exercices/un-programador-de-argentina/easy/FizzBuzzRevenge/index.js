8d69ca5c60d7721342dbf797605c34890d0b7897
Brayan Esteves <brayan.esteves93@gmail.com>
2023-11-22 21:50:42 -0400

    Un programador de Argetina (Task) - Revenge FizzBuzz [22/11/2023]

---
+++ src/exercices/un-programador-de-argentina/easy/FizzBuzzRevenge/index.js
@@ -0,0 +1,39 @@
+/**
+ * [ENGLISH]
+ * FizzBuzz Revenge.
+ * Create a function that has the same functionality as the previous <b>'FizzBuzz'</b>,
+ * but that receives as a parameter the words to be printed (Instead of 'Fizz' and 'Buzz')
+ * and the numbers that activate them and the maximum number of iterations.
+ * Expected output: fizzBuzz('Code', 'Project', 2, 7, 17)
+ * 1
+ * Code
+ * 3
+ * Code
+ * 5
+ * Code
+ * Project
+ * ...
+ * CodeProject
+ * 15
+ * Code
+ * 17
+ * =========
+ * [SPANISH]
+ * Venganza de FizzBuzz.
+ * Creá una función que tenga la misma funcionalidad que el <b>'FizzBuzz'</b> anterior,
+ * pero que reciba por parámetro las palabras a imprimir (En vez de <b>'Fizz'</b> y
+ * <b>'Buzz'</b>) y los números con los que se activan y el número máximo de iteraciones.
+ * Output esperado: fizzBuzz('Codigo', 'Proyecto', 2, 7, 17)
+ * 1
+ * Codigo
+ * 3
+ * Codigo
+ * 5
+ * Codigo
+ * Proyecto
+ * ...
+ * CodigoProyecto
+ * 15
+ * Codigo
+ * 17
+ */
